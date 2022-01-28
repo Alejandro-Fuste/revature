f5a580c3023f34b8f006612d9c34c86f7c4fe104
Alejandro F <afuste7@gmail.com>
2022-01-28 15:20:34 -0500

    added p3 project files

--- testJava/src/main/java/com/collectionoptions/arrays/ArrayBasics.java
+++ testJava/src/main/java/com/collectionoptions/arrays/ArrayBasics.java
@@ -1,25 +1,57 @@
 package com.collectionoptions.arrays;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Date;
+//import java.sql.Date;
 
 public class ArrayBasics {
-    public static void main(String[] args) {
-        int[] myArray = new int[]{1,2,3,4,5};
-        for(int x: myArray){
-            System.out.println(x);
-        }
+    public static void main(String[] args) throws ParseException {
+//        int[] myArray = new int[]{1,2,3,4,5};
+//        for(int x: myArray){
+//            System.out.println(x);
+//        }
+//
+//        System.out.println();
+//
+//        int[] mySecondArray = new int[4];
+//        mySecondArray[0] = 1;
+//        mySecondArray[1] = 2;
+//        mySecondArray[2] = 3;
+//        mySecondArray[3] = 4;
+//        for(int x: mySecondArray){
+//            System.out.println(x);
+//        }
+//
+//        System.out.println(Arrays.stream(myArray).average());
+//        LocalDate date = LocalDate.parse("2018-05-05");
+//        System.out.println(date);
+//        Date currentDate = new Date();
+////        long epoch = currentDate.getTime() / 1000;
+////        System.out.println("Epoch: " + epoch);
+//        System.out.println(currentDate);
 
-        System.out.println();
-
-        int[] mySecondArray = new int[4];
-        mySecondArray[0] = 1;
-        mySecondArray[1] = 2;
-        mySecondArray[2] = 3;
-        mySecondArray[3] = 4;
-        for(int x: mySecondArray){
-            System.out.println(x);
-        }
-
-        System.out.println(Arrays.stream(myArray).average());
+//        String str = "2022-01-25";
+////        SimpleDateFormat df = new SimpleDateFormat(str);
+//        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+//        Date date = df.parse(str);
+//        long epoch = date.getTime();
+//
+//
+//        Date nonSqlDate = new Date(epoch);
+////        Date dateSql = new Date(epoch);
+//        System.out.println(nonSqlDate);
     }
 }
+
+//    Please find the link to the panel assessment
+
+//Part - 1 : https://coding.revature.com/AuthenticateKey?id=69301ec041
+//        Part - 2 : https://coding.revature.com/AuthenticateKey?id=b40b882faa
+//
+//        Associates can take up the assessment during the training session on 01/26/2021[9:00 AM EST - 5:00 PM EST]
+
+//https://github.com/Alejandro-Fuste/JAVA_investing_app
+//https://github.com/revature-The-Jungle
