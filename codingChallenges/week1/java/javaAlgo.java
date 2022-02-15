4f2efbf8397cf466ebd5a6be53e3c7ee2b242da1
Alejandro F <afuste7@gmail.com>
2022-02-15 16:37:21 -0500

    added java code for coding challenge

--- codingChallenges/week1/java/javaAlgo.java
+++ codingChallenges/week1/java/javaAlgo.java
@@ -1,5 +1,42 @@
-public class javaAlgo {
+package algo.week1;
+
+import java.text.DecimalFormat;
+
+public class JavaAlgo {
+    public void actualMemorySize(String size){
+        double percentage = 0.07;
+
+        if (size.contains("GB")) {
+            String [] split = size.split("GB");
+            double sizeToDouble = Double.parseDouble(split[0]);
+            double result = sizeToDouble * percentage;
+            double newSize = sizeToDouble - result;
+            DecimalFormat df = new DecimalFormat("###.##");
+            String rounded = df.format(newSize);
+            System.out.println(rounded + "GB");
+        } else {
+            String[] splitMB = size.split("MB");
+            double sizeToDouble = Double.parseDouble(splitMB[0]);
+            double result = sizeToDouble * percentage;
+            double newSize = sizeToDouble - result;
+            DecimalFormat df = new DecimalFormat("###");
+            String rounded = df.format(newSize);
+            System.out.println(rounded + "MB");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println('test');
+        JavaAlgo case1 = new JavaAlgo();
+        case1.actualMemorySize("32GB"); // "29.76GB"
+
+        System.out.println("----------");
+
+        JavaAlgo case2 = new JavaAlgo();
+        case2.actualMemorySize("2GB"); // "1.86GB"
+
+        System.out.println("----------");
+
+        JavaAlgo case3 = new JavaAlgo();
+        case3.actualMemorySize("512MB"); // "476MB"
     }
 }
--- testJava/src/main/java/algo/week1/JavaAlgo.java
+++ testJava/src/main/java/algo/week1/JavaAlgo.java
@@ -1,8 +1,28 @@
 package algo.week1;
 
+import java.text.DecimalFormat;
+
 public class JavaAlgo {
     public void actualMemorySize(String size){
-        System.out.println(size);
+        double percentage = 0.07;
+
+        if (size.contains("GB")) {
+            String [] split = size.split("GB");
+            double sizeToDouble = Double.parseDouble(split[0]);
+            double result = sizeToDouble * percentage;
+            double newSize = sizeToDouble - result;
+            DecimalFormat df = new DecimalFormat("###.##");
+            String rounded = df.format(newSize);
+            System.out.println(rounded + "GB");
+        } else {
+            String[] splitMB = size.split("MB");
+            double sizeToDouble = Double.parseDouble(splitMB[0]);
+            double result = sizeToDouble * percentage;
+            double newSize = sizeToDouble - result;
+            DecimalFormat df = new DecimalFormat("###");
+            String rounded = df.format(newSize);
+            System.out.println(rounded + "MB");
+        }
     }
 
     public static void main(String[] args) {
@@ -14,10 +34,12 @@ public class JavaAlgo {
         JavaAlgo case2 = new JavaAlgo();
         case2.actualMemorySize("2GB"); // "1.86GB"
 
+        System.out.println("----------");
+
         JavaAlgo case3 = new JavaAlgo();
         case3.actualMemorySize("512MB"); // "476MB"
 
-        System.out.println("----------");
+
 
 
     }
