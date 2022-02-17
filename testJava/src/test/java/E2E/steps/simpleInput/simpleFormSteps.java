2ad577cbbdab8fa6b545985f5c98b926f0b6f33d
Alejandro F <afuste7@gmail.com>
2022-02-17 16:27:59 -0500

    updated pom.xml file, added chromedriver, added features and steps folders

---
+++ testJava/src/test/java/E2E/steps/simpleInput/simpleFormSteps.java
@@ -0,0 +1,43 @@
+package E2E.steps.simpleInput;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class simpleFormSteps {
+    @Given("the developer is on the demo selenium easy website")
+    public void theDeveloperIsOnTheDemoSeleniumEasyWebsite() {
+
+    }
+
+    @When("the developer enters a message into the input box")
+    public void theDeveloperEntersAMessageIntoTheInputBox() {
+
+    }
+
+    @When("the developer clicks on the show message button")
+    public void theDeveloperClicksOnTheShowMessageButton() {
+
+    }
+
+    @Then("the message typed in the input box will display on the page")
+    public void theMessageTypedInTheInputBoxWillDisplayOnThePage() {
+
+    }
+
+    @When("the developer enters a value into the a input box")
+    public void theDeveloperEntersAValueIntoTheAInputBox() {
+    }
+
+    @When("the developer enters a value into the b input box")
+    public void theDeveloperEntersAValueIntoTheBInputBox() {
+    }
+
+    @When("the developer clicks on the total button")
+    public void theDeveloperClicksOnTheTotalButton() {
+    }
+
+    @Then("the total will display on the page")
+    public void theTotalWillDisplayOnThePage() {
+    }
+}
