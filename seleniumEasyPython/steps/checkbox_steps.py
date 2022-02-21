1e093d59bc0c6e225ae17ba28d164df11077d784
Alejandro F <afuste7@gmail.com>
2022-02-21 18:03:06 -0500

    added steps for checkbox in python

---
+++ seleniumEasyPython/steps/checkbox_steps.py
@@ -0,0 +1,31 @@
+from behave import Given, When, Then
+
+
+@When(u'the developer clicks on single check box')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the developer clicks on single check box')
+
+
+@Then(u'a success message pops up on the screen')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then a success message pops up on the screen')
+
+
+@When(u'the developer clicks on first check box')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the developer clicks on first check box')
+
+
+@When(u'the developer clicks on second check box')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the developer clicks on second check box')
+
+
+@When(u'the developer clicks on third check box')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the developer clicks on third check box')
+
+
+@Then(u'the button text changes')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the button text changes')
