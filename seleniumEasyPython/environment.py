3af76f40d2cce8a1f68889e0e2396c16c0d431db
Alejandro F <afuste7@gmail.com>
2022-02-23 15:39:28 -0500

    added code to dynamic data POM and added POMs to environment file

--- seleniumEasyPython/environment.py
+++ seleniumEasyPython/environment.py
@@ -2,14 +2,21 @@ from behave.runner import Context
 from selenium import webdriver
 from poms.simple_input_POM import SimpleInputPOM
 from poms.checkbox_POM import CheckboxPOM
-# from poms.js_alerts_POM import
-# from poms.dynamic_data_POM import
+from poms.js_alerts_POM import JavascriptAlertsPOM
+from poms.dynamic_data_POM import DynamicDataPOM
 
 
 def before_all(context: Context):
+    # ------------ driver ------------
     context.driver = webdriver.Chrome("/Users/alejandro/Desktop/revature/seleniumEasyPython/chromedriver")
+
+    # ------------ POMs ------------
     context.simple_input = SimpleInputPOM(context.driver)
     context.checkbox = CheckboxPOM(context.driver)
+    context.js_alert = JavascriptAlertsPOM(context.driver)
+    context.dynamic_data = DynamicDataPOM(context.driver)
+
+    # ------------ wait and maximize window ------------
     context.driver.implicitly_wait(2)
     context.driver.maximize_window()
 
---
+++ seleniumEasyPython/poms/dynamic_data_POM.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+
+class DynamicDataPOM:
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    # ------------------- Get Random User --------------------------------
+
+    def get_new_user(self):
+        element: WebElement = self.driver.find_element(By.ID, 'save')
+        return element
+
+    def profile_image(self):
+        element: WebElement = self.driver.find_element(By.XPATH, "//*[contains(@src, 'https://randomuser.me/')]")
+        return element
+
+    def first_name(self):
+        element: WebElement = self.driver.find_element(By.XPATH, '//*[@id="loading"]/text()[1]')
+        return element
+
+    def last_name(self):
+        element: WebElement= self.driver.find_element(By.XPATH, '//*[@id="loading"]/text()[2]')
+        return element
