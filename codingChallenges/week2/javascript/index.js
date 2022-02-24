92f35a777065de2a4ca8dff0b317d7d2ae0cabba
Alejandro F <afuste7@gmail.com>
2022-02-24 11:41:36 -0500

    added variables to javascript challenge

---
+++ codingChallenges/week2/javascript/index.js
@@ -0,0 +1,14 @@
+const colorPatternTimes = (array) => {
+  // initialize variables for counter and array size
+  let colorTime = array.length * 2;
+  let counter = 0;
+
+  // loop w/ condition
+
+  // return switches + color time
+  return colorTime;
+};
+
+console.log(colorPatternTimes(["Blue"])); // -> 2
+console.log(colorPatternTimes(["Red", "Yellow", "Green", "Blue"])); // -> 11
+console.log(colorPatternTimes(["Blue", "Blue", "Blue", "Red", "Red", "Red"])); // -> 13
