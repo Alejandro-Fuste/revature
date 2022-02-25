6ec7d2bafcdc6bdb9932a554628468ea0bf635bc
Alejandro F <afuste7@gmail.com>
2022-02-25 17:22:09 -0500

    added progress bar feature and steps files in python

---
+++ seleniumEasyPython/features/progress_bar.feature
@@ -0,0 +1,6 @@
+Feature: Practice using selenium with the demo selenium easy website
+
+  Scenario: As a developer, I want to get practice testing a progress bar
+    Given the developer is on the progress bar demo selenium easy website
+    When the developer clicks on the download button
+    Then the developer will see one-hundred percentage on the page when the download completes
---
+++ seleniumEasyPython/steps/progress_bar_steps.py
@@ -0,0 +1,17 @@
+from behave import Given, When, Then
+
+
+@Given(u'the developer is on the progress bar demo selenium easy website')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the developer is on the progress bar demo selenium easy website')
+
+
+@When(u'the developer clicks on the download button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the developer clicks on the download button')
+
+
+@Then(u'the developer will see one-hundred percentage on the page when the download completes')
+def step_impl(context):
+    raise NotImplementedError(
+        u'STEP: Then the developer will see one-hundred percentage on the page when the download completes')
